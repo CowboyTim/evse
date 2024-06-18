ad2a949860d064dbe7bd78f29fda15bcc03cb21b
Tim Aerts <aardbeiplantje@gmail.com>
2024-06-18 21:38:54 +0200

    evse-ws-decrypt.sh: improve robustness + add awk/jq

--- evse-ws-decrypt.sh
+++ evse-ws-decrypt.sh
@@ -2,9 +2,10 @@
 fn=$1
 shift
 [[ ! -f "$fn" ]] && exit 1
+websocket_lua_script_file=$(dirname $(readlink -f $BASH_SOURCE))/ws.lua
 tshark -r "$fn" -2 -R \
     'tcp and (not tcp.len==0) and (websocket || http)' \
-    -Xlua_script:ws.lua \
+    -Xlua_script:$websocket_lua_script_file \
     -T fields \
     -E occurrence=l \
     -E separator=/t \
@@ -13,4 +14,5 @@ tshark -r "$fn" -2 -R \
     -e ip.dst \
     -e text \
     -e _ws.col.Info \
-    -e bcencrypt.command
+    -e bcencrypt.command \
+    |awk -F'\t' '{print $6}'|jq -cr
